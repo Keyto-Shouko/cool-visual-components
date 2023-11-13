2668b61ca778d2eaa1780d659ab9f45f4e3ab4bb
Bruno AC <balvesca@gmail.com>
2023-11-13 15:51:09 +0100

    updated gitignore and vite config to push correctly the lib

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,32 @@
-const path = require('path')
+const path = require("path");
 
 const isLibMode = process.argv.includes("--mode=lib");
 
 module.exports = {
-    build: {
-        ...(isLibMode ? {
-            lib: {
-                entry: path.resolve(__dirname, 'lib/index.js'),
-                name: 'CustomLib',
-                formats: ['es', 'umd'],
-                fileName: format => `lib.${format}.js`
-            }
-        } : {}),
-        rollupOptions: {
-            ...(!isLibMode ? {
-                input: {
-                    app: path.resolve(__dirname, 'index.html')
-                }
-            } : {
-                output: {
-                    assetFileNames: "lib.css"
-                }
-            }),
-        },
-        outDir: isLibMode ? "dist/lib" : "dist/app"
-    }
-}
+  build: {
+    ...(isLibMode
+      ? {
+          lib: {
+            entry: path.resolve(__dirname, "lib/index.js"),
+            name: "coolComponentLib",
+            formats: ["es", "umd"],
+            fileName: (format) => `lib.${format}.js`,
+          },
+        }
+      : {}),
+    rollupOptions: {
+      ...(!isLibMode
+        ? {
+            input: {
+              app: path.resolve(__dirname, "index.html"),
+            },
+          }
+        : {
+            output: {
+              assetFileNames: "lib.css",
+            },
+          }),
+    },
+    outDir: isLibMode ? "dist/lib" : "dist/app",
+  },
+};
